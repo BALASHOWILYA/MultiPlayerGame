80c135c86c9745217b422624eb8c46dd9d2bd224
Ilya <ilya.balashow1@gmail.com>
2022-01-06 00:49:48 +0600

    Added colour to a player

--- Assets/Scripts/MyNetworkManager.cs
+++ Assets/Scripts/MyNetworkManager.cs
@@ -5,17 +5,16 @@ using UnityEngine;
 
 public class MyNetworkManager : NetworkManager
 {
-    public override void OnClientConnect(NetworkConnection conn)
-    {
-        base.OnClientConnect(conn);
-
-        Debug.Log("I connected to a server!!!");
-    }
 
     public override void OnServerAddPlayer(NetworkConnection conn)
     {
         base.OnServerAddPlayer(conn);
-        
-        Debug.Log($"A player was added!!! Players are {numPlayers}");
+
+        MyNetworkPlayer player = conn.identity.GetComponent<MyNetworkPlayer>();
+
+        player.SetDisplayName($"Bob {numPlayers}");
+
+        player.SetDisplayColor(new Color(Random.Range(0f, 1f), (Random.Range(0f, 1f)), (Random.Range(0f, 1f))));
+    
     }
 }
